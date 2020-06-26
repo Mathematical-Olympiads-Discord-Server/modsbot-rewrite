4ece8adf03718830db28fabe3179d45ab828a1f3
IcosahedralDice <etomnz@gmail.com>
2020-06-27 02:15:37 +1200

    Activity logging

--- cogs/activity.py
+++ cogs/activity.py
@@ -14,19 +14,15 @@ today_messages = {}
 class Activity(Cog):
     def __init__(self, bot):
         self.bot = bot
-        schedule.every().day.at("12:00").do(asyncio.run_coroutine_threadsafe, self.process_today(), bot.loop)
+        schedule.every().day.at("12:10").do(asyncio.run_coroutine_threadsafe, self.process_today(), bot.loop).tag(
+            'cogs.activity')
 
     async def process_today(self):
         today_date = datetime.now().strftime("%d %b %Y")
         print(today_messages)
 
         # Figure out which users were active today.
-        active_users_today = ''
-        for user in today_messages:
-            if today_messages[user] > cfg.Config.config['daily_active_threshold']:
-                active_users_today += user
-                active_users_today += ' '
-        print(active_users_today)
+        active_users_today = str(today_messages)
 
         await self.bot.get_channel(cfg.Config.config['log_channel']).send(
             "Logged active users for {}: ```{}```".format(today_date, active_users_today))
@@ -34,18 +30,26 @@ class Activity(Cog):
         # Log that information.
         r_body = {'values': [[today_date, str(active_users_today)]]}
         cfg.Config.service.spreadsheets().values().append(spreadsheetId=cfg.Config.config['activity_sheet'],
-                                                          range='Sheet1!A1', valueInputOption='RAW',
+                                                          range='Log!A1', valueInputOption='RAW',
                                                           insertDataOption='INSERT_ROWS', body=r_body).execute()
 
         # Get the values of the previous 7 days.
 
+        # Clear today's
+        today_messages.clear()
+
     @Cog.listener()
     async def on_message(self, message):
-        if not message.author.bot:  # Ignore messages from bots
+        if not message.author.bot and message.guild is not None:  # Ignore messages from bots and DMs
             if message.author.id in today_messages:
                 today_messages[message.author.id] += 1
             else:
                 today_messages[message.author.id] = 1
+
+    @commands.command()
+    @commands.is_owner()
+    async def dump_activity(self, ctx):
+        await ctx.send(today_messages)
 
 
 def setup(bot):
