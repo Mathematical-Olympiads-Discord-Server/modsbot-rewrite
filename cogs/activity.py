538bb3c1e2a75b072316a455b5c52d087482623b
IcosahedralDice <etomnz@gmail.com>
2020-08-20 21:02:28 +1200

    top users

--- cogs/activity.py
+++ cogs/activity.py
@@ -209,7 +209,7 @@ class Activity(Cog):
         scores = [(x, int(score[x])) for x in score]
         scores.sort(key=lambda x: -x[1])
         embed = discord.Embed()
-        embed.add_field(name=f'Top users by activity score ({interval} day)',
+        embed.add_field(name=f'Top {users} users by activity score ({interval} day)',
                         value='\n'.join([f'`{i + 1}.` <@!{scores[i][0]}>: `{scores[i][1]}`' for i in range(users)]))
         await ctx.send(embed=embed)
 
