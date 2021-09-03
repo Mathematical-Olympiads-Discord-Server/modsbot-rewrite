0e0438ed8b47b26d126f251ac004cb4d74a4341e
Danie-1 <63882624+Danie-1@users.noreply.github.com>
2021-09-03 12:20:49 +0100

    Ping timeslot instead of mathjams

--- cogs/mathjams.py
+++ cogs/mathjams.py
@@ -10,19 +10,19 @@ class Mathjams(Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        schedule.every().friday.at("11:55").do(self.schedule_ping).tag('cogs.mathjams')
-        schedule.every().friday.at("19:55").do(self.schedule_ping).tag('cogs.mathjams')
-        schedule.every().saturday.at("02:55").do(self.schedule_ping).tag('cogs.mathjams')
-        self.mathjams_role = cfg.Config.config['mathjams_role']
+        schedule.every().friday.at("11:55").do(self.schedule_ping, timeslot="a").tag('cogs.mathjams')
+        schedule.every().friday.at("19:55").do(self.schedule_ping, timeslot="b").tag('cogs.mathjams')
+        schedule.every().saturday.at("02:55").do(self.schedule_ping, timeslot="c").tag('cogs.mathjams')
 
-    def schedule_ping(self):
-        self.bot.loop.create_task(self.ping_mathjams())
+    def schedule_ping(self, timeslot):
+        self.bot.loop.create_task(self.ping_mathjams(timeslot))
 
-    async def ping_mathjams(self):
+    async def ping_mathjams(self, timeslot):
+        role_id = cfg.Config.config[f'mathjams_slot_{timeslot}']
         mathjams_channel = await self.bot.fetch_channel(cfg.Config.config['mathjams_channel'])
-        r = self.bot.get_guild(cfg.Config.config['mods_guild']).get_role(cfg.Config.config['mathjams_role'])
+        r = self.bot.get_guild(cfg.Config.config['mods_guild']).get_role(role_id)
         await r.edit(mentionable=True)
-        await mathjams_channel.send('Mathjams in 5 minutes XD! <@&{}>'.format(cfg.Config.config['mathjams_role']))
+        await mathjams_channel.send('Mathjams in 5 minutes XD! <@&{}>'.format(role_id))
         await r.edit(mentionable=False)
 
 
