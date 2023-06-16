9df488b78f2928841f143aa7aebe6f7bee94087b
KitKung <cyw00009@yahoo.com.hk>
2023-06-16 23:06:47 +0800

    add mock_custom

--- cogs/potd.py
+++ cogs/potd.py
@@ -5,6 +5,7 @@ import openpyxl.utils
 import random
 import statistics
 from datetime import datetime, timedelta
+import re
 
 import discord
 import schedule
@@ -640,10 +641,12 @@ class Potd(Cog):
         else:
             genre_rule = ["ACGN"] * len(difficulty_bounds)
 
+        # pick the genre of each problem
         genres=[]
-        while not self.is_genre_legit(genres, template, difficulty_bounds, genre_rule):
+        while not self.is_genre_legit(genres, template, genre_rule):
             genres=list(map(lambda x: random.choice(x),genre_rule))
 
+        # set up variables
         problems_tex = []
         potds = cfg.Config.service.spreadsheets().values().get(spreadsheetId=cfg.Config.config['potd_sheet'],
                                                                range=POTD_RANGE).execute().get('values', [])
@@ -674,6 +677,60 @@ class Potd(Cog):
         else: # 1-day contests
             await self.send_out_mock(ctx, template, problems_tex)
 
+    @commands.command(aliases=['mock_custom', 'custom_mock'], brief='Create a custom mock paper using past POTDs.',
+        help='`-mock_custom [5 7] [7 9] [9 11] [5 7] [7 9] [9 11]`: create a mock paper where problem 1 is d5-7, problem 2 is d7-9, etc.\n'
+            '`-mock_custom [3 4 G] [4 5 G] [5 6 G] [6 7 G]`: create a mock paper where problem 1 is d3-4 geometry, problem 2 is d4-5 geometry, etc.')
+    @commands.cooldown(1, 30, BucketType.user)
+    async def potd_mock_custom(self, ctx, *, rules):
+        # parse the user inputed rules
+        parsed_rules = self.parse_mock_rules(rules)
+
+        # handle garbage or too long input
+        if parsed_rules == False:
+            await ctx.send("Custom rule input error! Please input the custom rule like this: `[5 7] [7 9] [9 11]`.")
+            return
+        if len(parsed_rules) > 15:
+            await ctx.send("Maximum number of problems allowed is 15.")
+            return
+
+        # get the genre rule
+        genre_rule = []
+        for parsed_rule in parsed_rules:
+            if parsed_rule['genres'] == '':
+                genre_rule.append('ACGN')
+            else:
+                genre_rule.append(parsed_rule['genres'])
+
+        # pick the genre of each problem
+        genres=[]
+        while not self.is_genre_legit(genres, "Custom", genre_rule):
+            genres=list(map(lambda x: random.choice(x),genre_rule))
+
+        # get the difficulty bounds
+        difficulty_bounds = []
+        for parsed_rule in parsed_rules:
+            difficulty_bounds.append([parsed_rule['diff_lower'], parsed_rule['diff_upper']])
+
+        # set up variables
+        problems_tex = []
+        potds = cfg.Config.service.spreadsheets().values().get(spreadsheetId=cfg.Config.config['potd_sheet'],
+                                                               range=POTD_RANGE).execute().get('values', [])
+        already_picked = []
+        parsed_rules_string = self.stringify_mock_rules(parsed_rules)
+
+        # render the mock paper
+        try:
+            for i in range(0,len(difficulty_bounds)):
+                picked_potd = self.pick_potd(difficulty_bounds[i][0], difficulty_bounds[i][1], genres[i], potds, already_picked, ctx, True)
+                already_picked.append(picked_potd)
+                potd_statement = self.get_potd_statement(int(picked_potd), potds)
+                problems_tex.append(f'\\textbf{{Problem {i+1}. (POTD {str(picked_potd)})}}\\\\ ' + potd_statement)
+
+            await ctx.send(f'<@{ctx.author.id}> Custom Mock created ({parsed_rules_string})')
+            await self.send_out_mock(ctx, '(Custom)', problems_tex)
+        except:
+            await ctx.send(f'Unable to create mock paper according to custom rule ({parsed_rules_string})')
+
     async def send_out_mock(self, ctx, name, problems_tex):
         while len(problems_tex) > 0: # still has problems to send out
             title = r'\begin{center}\textbf{\textsf{MODSBot Mock ' + name + r'}}\end{center}'
@@ -684,14 +741,17 @@ class Potd(Cog):
             to_tex = f'<@419356082981568522>\n```tex\n {title} {problems}```'
             await ctx.send(to_tex, delete_after=5) 
 
-    def is_genre_legit(self, genres, template, difficulty_bounds, genre_rule):
-        if len(genres) != len(difficulty_bounds):
+    def is_genre_legit(self, genres, template, genre_rule):
+        if len(genres) != len(genre_rule):
             return False
         
-        # the paper need to contain all genres listed in genre_rule
-        for genre in set(reduce(lambda x, y: x+y, genre_rule)):
-            if not genre in genres:
-                return False
+        # the paper should cover as many genre listed in genre_rule as possible
+        question_number = len(genre_rule)
+        different_genre_number = len(set(''.join(genre_rule)))
+        genres_needed = min(question_number, different_genre_number)
+
+        if len(genres) < genres_needed:
+            return False
 
         # the selected genres need to match the genre_rule
         for i in range(0,len(genres)):
@@ -713,7 +773,7 @@ class Potd(Cog):
             genres_geoff_smith = [genres[index] for index in [0,1,3,4]]
             if not ("A" in genres_geoff_smith and "C" in genres_geoff_smith and "G" in genres_geoff_smith and "N" in genres_geoff_smith):
                 return False
-
+            
         return True
 
     def pick_potd(self, diff_lower_bound_filter, diff_upper_bound_filter, genre_filter, potds, already_picked, ctx, search_unsolved:bool):
@@ -775,6 +835,56 @@ class Potd(Cog):
             return potd_statement
         except IndexError:
             return None
+        
+    def parse_mock_rules(self, rules):
+        parsed_rules = []
+
+        rules = rules.replace(",", " ")
+        res = re.findall(r'\[.*?\]', rules) 
+
+        for substring in res:
+            modified_substring = substring[1:-1].split(' ')
+
+            if len(modified_substring) not in [2,3]:
+                return False
+            if len(modified_substring) == 2:
+                modified_substring.append('ACGN')
+
+            try:
+                int(modified_substring[0])
+                int(modified_substring[1])
+            except:
+                return False
+            if int(modified_substring[0]) > int(modified_substring[1]):
+                return False
+            
+            diff_lower = max(int(modified_substring[0]), 0)
+            diff_upper = min(int(modified_substring[1]), 14)
+            genres = ''
+            possible_genres = ['A', 'C', 'G', 'N']
+            for char in modified_substring[2]:
+                if char.upper() in possible_genres and char.upper() not in genres:
+                    genres += char.upper()
+            
+            parsed_rule = {
+                'diff_lower': diff_lower,
+                'diff_upper': diff_upper,
+                'genres': genres
+            }
+
+            parsed_rules.append(parsed_rule)
+        
+        return parsed_rules
+
+    def stringify_mock_rules(self, parsed_rules):
+        rule_strings = []
+        for parse_rule in parsed_rules:
+            if parse_rule['genres'] not in ['', 'ACGN']:
+                rule_string = f"[{parse_rule['diff_lower']} {parse_rule['diff_upper']} {parse_rule['genres']}]"
+            else:
+                rule_string = f"[{parse_rule['diff_lower']} {parse_rule['diff_upper']}]"
+            rule_strings.append(rule_string)
+        return ' '.join(rule_strings)
 
     @commands.command(aliases=['mark'], brief='Mark the POTD you have solved')
     @commands.cooldown(1, 5, BucketType.user)
