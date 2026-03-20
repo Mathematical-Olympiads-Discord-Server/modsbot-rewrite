116ca7d3630787f88d4293794da6a1390be17695
yumeiJP <tk5138@email.kist.ed.jp>
2026-03-20 21:10:59 +0900

    add JPMO

--- cogs/mock.py
+++ cogs/mock.py
@@ -65,6 +65,7 @@ class Mock(Cog):
             "USAJMO",
             "JMO",
             "CHINA",
+            "JPMO",
         ]
         if template not in template_list and template != "AFMO":
             await ctx.send(
@@ -111,6 +112,8 @@ class Mock(Cog):
                 difficulty_bounds = [[4, 5], [5, 6], [6, 7], [7, 8], [8, 9]]
             elif template in {"USAJMO", "JMO"}:
                 difficulty_bounds = [[3, 5], [5, 7], [7, 8], [3, 5], [5, 7], [7, 8]]
+            elif template == "JPMO":
+                difficulty_bounds = [[4,5], [5,6], [6,7], [7,8], [8,9]]
         # SMO2 seems to have an unspoken rule to start with geometry at P1 and nowhere
         # else
         if template == "SMO2":
