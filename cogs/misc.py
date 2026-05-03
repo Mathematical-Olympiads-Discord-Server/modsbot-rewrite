ddb8cb537d34b126f2fb510b481b9a4985225d3d
yumeiJP <yumei3751@gmail.com>
2026-05-03 11:10:08 +0900

    black

--- cogs/misc.py
+++ cogs/misc.py
@@ -39,7 +39,7 @@ class Misc(Cog):
     def record(self):
         # TODO: work out what to do with this
         g = self.bot.get_guild(cfg.Config.config["mods_guild"])  # noqa: F841
-    
+
     async def delete_recent_messages(self, user: discord.Member):
         trap_window_minutes = cfg.Config.config["trap_window_minutes"]
 
@@ -49,22 +49,24 @@ class Misc(Cog):
 
         for channel in guild.text_channels:
             perms = channel.permissions_for(guild.me)
-            if not perms.manage_messages or not perms.read_message_history: continue
+            if not perms.manage_messages or not perms.read_message_history:
+                continue
 
             batch = []
 
             async for message in channel.history(limit=None, after=cutoff):
-                if message.author.id != user.id: continue
+                if message.author.id != user.id:
+                    continue
 
                 batch.append(message)
-            
+
             for msg in batch:
                 try:
                     await msg.delete()
                     deleted_count += 1
-                except(discord.NotFound, discord.Forbidden):
+                except (discord.NotFound, discord.Forbidden):
                     pass
-        
+
         print(f"Delted {deleted_count} messages from {user}")
 
     @Cog.listener()
@@ -206,16 +208,21 @@ class Misc(Cog):
             except discord.Forbidden:
                 pass
             return"""
-        
+
         trap_channel_id = cfg.Config.config["trap_channel_id"]
-        trap_timeout_duration_minutes = cfg.Config.config["trap_timeout_duration_minutes"]
+        trap_timeout_duration_minutes = cfg.Config.config[
+            "trap_timeout_duration_minutes"
+        ]
         staff_ids = cfg.Config.config.get("staff", [])
 
-        if message.channel.id == trap_channel_id and not message.author.bot and message.author.id not in staff_ids:
+        if (
+            message.channel.id == trap_channel_id
+            and not message.author.bot
+            and message.author.id not in staff_ids
+        ):
             member = message.author
             await member.timeout(timedelta(minutes=trap_timeout_duration_minutes))
             asyncio.create_task(self.delete_recent_messages(message.author))
-
 
     @commands.command()
     @commands.guild_only()
